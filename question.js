ff01a5274b1b9df70c2a4bcbe36c55d56f00bd56
Pritam Dey <76156595+HyperPritu@users.noreply.github.com>
2021-08-24 01:10:30 +0530

    Add files via upload

---
+++ question.js
@@ -0,0 +1,27 @@
+let question1 = 
+    {
+        ques: "Pepsi is a...",
+        op1: "1. Gandu",
+        op2: "2. 2 Rupee Human",
+        op3: "3. Elephant",
+        op4: "4. All of the above",
+        correct: "4. All of the above"
+    }
+let question2 = 
+    {
+        ques: "Rohan is a...",
+        op1: "1. Topper",
+        op2: "2. Noob",
+        op3: "3. Mediocre",
+        op4: "4. Illeterate",
+        correct: "1. Topper"
+    }
+let question3 = 
+    {
+        ques: "Sun rises from...",
+        op1: "1. East",
+        op2: "2. West",
+        op3: "3. North",
+        op4: "4. South",
+        correct: "1. East"
+    }
